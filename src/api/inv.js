707c681ee354f1b4cba85477b61600f8d240b375
kasunmahee34-oss <kasunmahee34@gmail.com>
2026-09-14 12:01:15 +0530

    add base url to code

--- src/api/inv.js
+++ src/api/inv.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE = import.meta.env.VITE_API_BASE;
+const BASE = 'https://synora-inv-back-production.up.railway.app/api/';
 const api = axios.create({ baseURL: BASE });
 
 api.interceptors.request.use((config) => {
